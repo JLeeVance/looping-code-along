5303b18d898e6d1b5a73d4e1ab14828d5fb92c1d
JustinLeeVance <justinleevance94@gmail.com>
2023-11-30 22:57:39 -0500

    Looping Code Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const arrayOfStringNames = ["Bob" , "Nancy" , "Joe"];
+
+function writeCards(arrayOfStringNames , eventName) {
+    const newArray = [];
+    for ( let i = 0 ; i < arrayOfStringNames.length ; i++) {
+        let message = `Thank you, ${arrayOfStringNames[i]}, for the wonderful ${eventName} gift!`;
+        newArray.push(message);
+    };
+    return newArray;
+};
+
+function countDown (int) {
+    let i = int;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    };
+};
+countDown(4);
